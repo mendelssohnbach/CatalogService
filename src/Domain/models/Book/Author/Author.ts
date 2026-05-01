31f4fdd7e82876ffcda4347f30d517d404b1ad19
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-05-01 16:50:56 +0900

    P111 文字列クォート間違いを正す

--- src/Domain/models/Book/Author/Author.ts
+++ src/Domain/models/Book/Author/Author.ts
@@ -12,7 +12,7 @@ export class Author extends ValueObject<AuthorValue, 'Author'> {
   protected validate(value: AuthorValue): void {
     if (value.length < Author.MIN_LENGTH || value.length > Author.MAX_LENGTH) {
       throw new Error(
-        '著者名は${Author.MIN_LENGTH}文字以上、${Author.MAX_LENGTH}文字以下でなければなりません。'
+        `著者名は${Author.MIN_LENGTH}文字以上、${Author.MAX_LENGTH}文字以下でなければなりません。`
       );
     }
   }
