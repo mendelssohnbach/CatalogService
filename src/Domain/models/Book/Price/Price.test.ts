a44f7793557a6f1d06b15dafb2b3c61d78039ce8
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2026-05-02 14:40:18 +0900

    it を test に統一する

--- src/Domain/models/Book/Price/Price.test.ts
+++ src/Domain/models/Book/Price/Price.test.ts
@@ -2,7 +2,7 @@ import { Price } from './Price';
 
 describe('Price', () => {
   // 正常系
-  it('正しい値と通貨コードJPYで有効なPriceを作成する', () => {
+  test('正しい値と通貨コードJPYで有効なPriceを作成する', () => {
     const validAmount = 500;
     const price = new Price({ amount: validAmount, currency: 'JPY' });
     expect(price.amount).toBe(validAmount);
@@ -10,7 +10,7 @@ describe('Price', () => {
   });
 
   // 異常系
-  it('無効な通貨コードの場合エラーを投げる', () => {
+  test('無効な通貨コードの場合エラーを投げる', () => {
     const invalidCurrency = 'USD';
     expect(() => {
       // @ts-expect-error テストのために無効な値を渡す
@@ -18,14 +18,14 @@ describe('Price', () => {
     }).toThrow('現在は日本円のみを扱います。');
   });
 
-  it('MIN未満の値でPriceを生成するとエラーを投げる', () => {
+  test('MIN未満の値でPriceを生成するとエラーを投げる', () => {
     const lessThanMin = Price.MIN - 1;
     expect(() => {
       new Price({ amount: lessThanMin, currency: 'JPY' });
     }).toThrow(`価格は${Price.MIN}円から${Price.MAX}円の間でなければなりません。`);
   });
 
-  it('MAX超の値でPriceを生成するとエラーを投げる', () => {
+  test('MAX超の値でPriceを生成するとエラーを投げる', () => {
     const moreThanMax = Price.MAX + 1;
     expect(() => {
       new Price({ amount: moreThanMax, currency: 'JPY' });
